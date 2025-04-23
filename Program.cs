30e6b69d8c4293477b141f9e7ce6a69fbeaab0b0
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-04-23 19:31:46 +0200

    Update README.md

--- Program.cs
+++ Program.cs
@@ -9,20 +9,18 @@
 // <date>20.04.2025 10:43:36</date>
 //
 // <summary>
-// Konsolen Applikation mit Menü
+// Konsolen Applikation für Beispiele mit ZIP Archiv
 // </summary>
-//
-// <remarks>
-// https://stackoverflow.com/questions/22604941/how-can-i-unzip-a-file-to-a-net-memory-stream
-// </remarks>
 //-----------------------------------------------------------------------
 
 namespace Console.ReadStreamFromZip
 {
     using System;
     using System.Collections.Generic;
+    using System.Data;
     using System.IO;
     using System.IO.Compression;
+    using System.Text;
 
     public class Program
     {
@@ -33,11 +31,16 @@ namespace Console.ReadStreamFromZip
             /* Pfad der EXE-Datei*/
             exePath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
 
+            CreateDemoFiles();
+
             do
             {
                 Console.Clear();
-                Console.WriteLine("1. Create Zip File");
-                Console.WriteLine("2. Menüpunkt 2");
+                Console.WriteLine("1. Erstellen Zip File");
+                Console.WriteLine("2. Lesen Zip File");
+                Console.WriteLine("3. Update Zip File");
+                Console.WriteLine("4. Löschen einer Datei im Zip File");
+                Console.WriteLine("5. Hinzufügen einer Datei zum Zip File");
                 Console.WriteLine("X. Beenden");
 
                 Console.WriteLine("Wählen Sie einen Menüpunkt oder 'x' für beenden");
@@ -56,64 +59,157 @@ namespace Console.ReadStreamFromZip
                     {
                         MenuPoint2();
                     }
+                    else if (key == ConsoleKey.D3)
+                    {
+                        MenuPoint3();
+                    }
+                    else if (key == ConsoleKey.D4)
+                    {
+                        MenuPoint4();
+                    }
+                    else if (key == ConsoleKey.D5)
+                    {
+                        MenuPoint5();
+                    }
                 }
             }
             while (true);
         }
 
+        private static void CreateDemoFiles()
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                string demoDatei = Path.Combine(exePath, $"Datei_{i}.txt");
+                if (File.Exists(demoDatei) == false)
+                {
+                    File.WriteAllText(demoDatei, $"Inhalt von Datei {i}");
+                }
+            }
+        }
+
         private static void MenuPoint1()
         {
             Console.Clear();
-
-            IEnumerable<string> files = new DirectoryInfo(exePath).GetFiles("*.txt").Select(s => s.FullName);
+            Console.WriteLine("Neues ZIP Archiv erstellen");
 
             string zipName = Path.Combine(exePath, "ZipContext.zip");
-            ZipHelper.CreateZipFile(zipName, files);
+            IEnumerable<string> files = new DirectoryInfo(exePath).GetFiles("*.txt").Select(s => s.FullName);
 
+            if (File.Exists(zipName))
+            {
+                File.Delete(zipName);
+            }
+
+            using (ZipArchive archive = ZipFile.Open(zipName, ZipArchiveMode.Create))
+            {
+                archive.Comment = "Kommentar zur Archiv";
+                foreach (var fPath in files)
+                {
+                    archive.CreateEntryFromFile(fPath, Path.GetFileName(fPath));
+                }
+            }
+
+            Console.WriteLine($"ZIP Archiv {Path.GetFileName(zipName)} erstellt.");
+            Console.WriteLine("Mit einer beliebigen Taste zum Menü");
             Console.ReadKey();
         }
 
         private static void MenuPoint2()
         {
             Console.Clear();
+            Console.WriteLine("ZIP Archiv lesen");
 
             string zipName = Path.Combine(exePath, "ZipContext.zip");
-            using (var file = File.OpenRead(zipName))
+            using (ZipArchive archive = ZipFile.Open(zipName, ZipArchiveMode.Read))
             {
-                using (var zip = new ZipArchive(file, ZipArchiveMode.Read))
+                archive.Comment = "Test Archiv";
+                foreach (var entry in archive.Entries)
                 {
-                    foreach (var entry in zip.Entries)
+                    using (StreamReader sr = new StreamReader(entry.Open()))
                     {
-                        using (StreamReader sr = new StreamReader(entry.Open()))
-                        {
-                            Console.WriteLine(sr.ReadToEnd());
-                        }
+                        string archivFile = entry.Name;
+                        Console.WriteLine(sr.ReadToEnd());
+                        Console.WriteLine($"Datei {archivFile}.");
                     }
                 }
             }
 
-           Console.ReadKey();
+            Console.WriteLine("Mit einer beliebigen Taste zum Menü");
+            Console.ReadKey();
         }
-    }
 
-    public static class ZipHelper
-    {
-        /// <summary>
-        /// Create a ZIP file of the files provided.
-        /// </summary>
-        /// <param name="fileName">The full path and name to store the ZIP file at.</param>
-        /// <param name="files">The list of files to be added.</param>
-        public static void CreateZipFile(string fileName, IEnumerable<string> files)
+        private static void MenuPoint3()
         {
-            // Create and open a new ZIP file
-            var zip = ZipFile.Open(fileName, ZipArchiveMode.Create);
-            foreach (var file in files)
+            Console.Clear();
+            Console.WriteLine("ZIP Archiv updaten");
+
+            string zipName = Path.Combine(exePath, "ZipContext.zip");
+            using (ZipArchive archive = ZipFile.Open(zipName, ZipArchiveMode.Update))
             {
-                // Add the entry for each file
-                zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+                archive.Comment = "Update ZIP Archiv";
+
+                StringBuilder document = null;
+                var entry = archive.GetEntry("Datei_1.txt");
+                using (StreamReader reader = new StreamReader(entry.Open()))
+                {
+                    if (document == null)
+                    {
+                        document = new StringBuilder();
+                        document.AppendLine(reader.ReadToEnd());
+                    }
+                }
+
+                entry.Delete();
+                entry = archive.CreateEntry("Datei_1.txt");
+                entry.Comment = $"Update {entry.Name}";
+                document.AppendLine("Inhalt von Datei 2");
+                document.AppendLine("Inhalt von Datei 3");
+                document.AppendLine("Inhalt von Datei 4");
+                document.AppendLine("Inhalt von Datei 5");
+
+                using (StreamWriter writer = new StreamWriter(entry.Open()))
+                {
+                    writer.Write(document);
+                }
             }
-            // Dispose of the object when we are done
-            zip.Dispose();
+
+            Console.WriteLine("Mit einer beliebigen Taste zum Menü");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint4()
+        {
+            Console.Clear();
+            Console.WriteLine("ZIP Archiv, Datei entfernen");
+
+            string zipName = Path.Combine(exePath, "ZipContext.zip");
+            using (ZipArchive archive = ZipFile.Open(zipName, ZipArchiveMode.Update))
+            {
+                archive.Comment = "Update ZIP Archiv";
+                var entry = archive.GetEntry("Datei_1.txt");
+                entry.Delete();
+            }
+
+            Console.WriteLine("Mit einer beliebigen Taste zum Menü");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint5()
+        {
+            Console.Clear();
+            Console.WriteLine("ZIP Archiv, Datei hinzufügen");
+
+            string zipName = Path.Combine(exePath, "ZipContext.zip");
+            string datei = Path.Combine(exePath, "Datei_3.txt");
+            using (ZipArchive archive = ZipFile.Open(zipName, ZipArchiveMode.Update))
+            {
+                archive.Comment = "Hinzufügen einer Datei zum ZIP Archiv";
+                archive.CreateEntryFromFile(datei, Path.GetFileName(datei));
+            }
+
+            Console.WriteLine("Mit einer beliebigen Taste zum Menü");
+            Console.ReadKey();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Console.ReadStreamFromZip
+
+![NET](https://img.shields.io/badge/NET-8.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2022](https://img.shields.io/badge/Visual%20Studio-2022-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2025.0-yellow.svg)]
+
+Das Beispiel zeigt verschiedene Möglichkeiten im Umgang mit einem ZIP Archiv mit NET Core.</br>
+
+<img src="ConsoleZIP.png" style="width:750px;"/></br>
+
+Beispiele:</br>
+- Erstellen eines ZIP Archiv
+- Lesen eines ZIP Archiv
+- Update eines ZIP Archiv
+  Eine Datei Ändern ohne diese auszupacken
+- Löschen einer Datei aus dem ZIP Archiv
+- Hinzufügen einer neuen Datei zum ZIP Archive
